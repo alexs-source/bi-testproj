ed0a569ba90a59c257d5b533710dd2ba1eee80c9
alexs-source <alexander.schwamberger@outlook.de>
2020-08-22 11:03:19 +0200

    DbConnectionHelper

---
+++ Utils/DbConnectioHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bi_testproj.Utils
+{
+    public class DbConnectioHelper
+    {
+
+    }
+}
